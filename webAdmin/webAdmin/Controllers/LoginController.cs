dc404c70805c97b53bb6769b1e933afdbe9ef77e
Hồ Huỳnh Lâm <huynhlam1001@gmail.com>
2015-05-29 17:23:18 +0700

    abc

---
+++ webAdmin/webAdmin/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webAdmin.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var tkData = new QLBanHangEntities().TaiKhoans;
+                var count = tkData.Count(x => x.ID.ToLower() == model.UserName.ToLower() && x.PW == model.Password);
+                if (count > 0)
+                {
+                    //return RedirectToAction("Index", "Home");
+                    return View();
+                }
+            }
+            return View();
+        }
+
+    }
+}
